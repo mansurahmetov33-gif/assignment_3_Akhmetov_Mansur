3d295d41a71784af70c9cc452e716621d385dad5
userp <mansurahmetov3@gmail.com>
2026-01-24 19:11:41 +0500

    add BaseEntity

--- src/model/BaseEntity.java
+++ src/model/BaseEntity.java
@@ -1,7 +1,40 @@
 package model;
 
 public abstract class BaseEntity {
+    protected int id;
+    protected String name;
 
+    public BaseEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public abstract String getEntityType();
+    public abstract String getDetails();
+
+    public String basicInfo() {
+        return id + ": " + name;
+    }
+
+    // getters / setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
 }
 
 

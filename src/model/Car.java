6d1e78d3f75b7e3f6290e11f6bb7e7abc337ae3d
userp <mansurahmetov3@gmail.com>
2026-01-24 19:11:53 +0500

    add Car

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Car extends BaseEntity {
+    private double pricePerDay;
 
+    public Car(int id, String name, double pricePerDay) {
+        super(id, name);
+        this.pricePerDay = pricePerDay;
+    }
+
+    @Override
+    public String getEntityType() {
+        return "Car";
+    }
+
+    @Override
+    public String getDetails() {
+        return "Car: " + name + ", price per day: " + pricePerDay;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        if (pricePerDay <= 0) {
+            throw new IllegalArgumentException("Price must be positive");
+        }
+        this.pricePerDay = pricePerDay;
+    }
 }

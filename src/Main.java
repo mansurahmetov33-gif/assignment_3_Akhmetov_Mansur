53e3ba137d43e21df2522ec7d7956d0be928091c
userp <mansurahmetov3@gmail.com>
2026-01-24 19:18:37 +0500

    add Main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
-void main() {
-    
+import model.Car;
+import repository.CarRepository;
+import repository.impl.CarRepositoryImpl;
+import service.CarService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CarRepository carRepository = new CarRepositoryImpl();
+        CarService carService = new CarService(carRepository);
+
+        carService.addCar(new Car(0, "Toyota", 50.0));
+        carService.addCar(new Car(0, "BMW", 80.0));
+
+        carService.getAllCars().forEach(System.out::println);
+    }
 }

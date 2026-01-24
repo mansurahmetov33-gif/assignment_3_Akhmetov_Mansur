48599dfbcb2ae1de88529ec09ed45726a5877cb7
userp <mansurahmetov3@gmail.com>
2026-01-24 19:13:18 +0500

    add Rental  model

--- src/model/Rental.java
+++ src/model/Rental.java
@@ -1,6 +1,20 @@
 package model;
 
 public class Rental {
+    private int id;
+    private Car car;
+    private Customer customer;
+    private int days;
 
+    public Rental(int id, Car car, Customer customer, int days) {
+        this.id = id;
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public double calculateTotalPrice() {
+        return car.getPricePerDay() * days;
+    }
 }
 

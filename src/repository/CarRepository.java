4d1cf34e5df880beafa253611f75e71174129d41
userp <mansurahmetov3@gmail.com>
2026-01-31 14:38:36 +0500

    Implement CarRepository using JDBC

--- src/repository/CarRepository.java
+++ src/repository/CarRepository.java
@@ -2,8 +2,8 @@ package repository;
 
 import model.Car;
 import java.util.List;
-
-public interface CarRepository {
+import repository.interfaces.Repository;
+public interface CarRepository extends Repository<Car> {
     void save(Car car);
     Car findById(int id);
     List<Car> findAll();

00d180b8d49f2167e5f5fb3406a7d59b9368233e
userp <mansurahmetov3@gmail.com>
2026-01-31 14:36:45 +0500

    Add Engine model

---
+++ src/model/Engine.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Engine {
+
+    private String type;
+    private int horsepower;
+
+    public Engine(String type, int horsepower) {
+        this.type = type;
+        this.horsepower = horsepower;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    @Override
+    public String toString() {
+        return type + " (" + horsepower + " HP)";
+    }
+}

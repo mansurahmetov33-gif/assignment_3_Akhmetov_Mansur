50aa7b0c9d72e40d8bae2c28d9bb45f673de5998
userp <mansurahmetov3@gmail.com>
2026-02-02 22:49:57 +0500

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,54 @@
 import model.Car;
+import model.Engine;
 import repository.CarRepository;
 import repository.impl.CarRepositoryImpl;
-import service.CarService;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
         CarRepository carRepository = new CarRepositoryImpl();
-        CarService carService = new CarService(carRepository);
 
-        carService.addCar(new Car(0, "Toyota", 50.0));
-        carService.addCar(new Car(0, "BMW", 80.0));
+        // Create engines
+        Engine engine1 = new Engine("Petrol", 150);
+        Engine engine2 = new Engine("Diesel", 120);
 
-        carService.getAllCars().forEach(System.out::println);
+        // Create cars
+        Car car1 = new Car(1, "Toyota", 50.0, engine1);
+        Car car2 = new Car(2, "BMW", 80.0, engine2);
+
+        // Save to database
+        carRepository.save(car1);
+        carRepository.save(car2);
+
+        // Get all cars
+        System.out.println("All cars:");
+        List<Car> cars = carRepository.findAll();
+        cars.forEach(c -> System.out.println(c.getDetails()));
+
+        // Find by id
+        System.out.println("\nFind car with id = 1:");
+        Car found = carRepository.findById(1);
+        if (found != null) {
+            System.out.println(found.getDetails());
+        }
+
+        // Update car
+        if (found != null) {
+            found.setPricePerDay(60.0);
+            carRepository.update(found);
+            System.out.println("\nUpdated car:");
+            System.out.println(found.getDetails());
+        }
+
+        // Delete car
+        carRepository.delete(2);
+        System.out.println("\nAfter delete:");
+
+        carRepository.findAll()
+                .forEach(c -> System.out.println(c.getDetails()));
     }
 }
+
